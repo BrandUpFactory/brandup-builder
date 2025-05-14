24df985671708118a4302f70c4dcbe3e236a70eb
BrandUpFactory <meyer@brandupfactory.com>
2025-05-14 19:07:27 +0200

    14_05_25-23

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
+  eslint: {
+    ignoreDuringBuilds: true, // ✅ Ignoriert ESLint-Warnungen beim Vercel-Build
+  },
+}
 
-export default nextConfig;
+export default nextConfig
